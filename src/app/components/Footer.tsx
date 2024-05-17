fcda69dd5909763819eb235a9d81700c24e6b7f0
LapinGit <lapingithub@gmail.com>
2024-05-17 15:13:21 +0800

    code readability

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,18 +5,20 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-amber-300 h-12 md:h-24 p-4 lg:px-20 xl:px-40 text-red-500 flex items-center justify-between ">
-      
-         <div className=" md:flex gap-4 flex-1 items-center font-bold text-xl ">
-         <Image className="rounded-full md:flex hidden" src="/AngelsBurgerLogo.png" alt="" width={90} height={90} /> 
-         <p > Angel&apos;s Burger</p>   
-         </div>
+      <div className=" md:flex gap-4 flex-1 items-center font-bold text-xl ">
+        <Image
+          className="rounded-full md:flex hidden"
+          src="/AngelsBurgerLogo.png"
+          alt=""
+          width={90}
+          height={90}
+        />
+        <p> Angel&apos;s Burger</p>
+      </div>
 
-    <div className="  lg:text-base	  ">
-    <p> © ALL RIGHTS RESERVED.  </p>
-</div>
-
-   
-    
+      <div className="  lg:text-base	  ">
+        <p> © ALL RIGHTS RESERVED. </p>
+      </div>
     </div>
   );
 };
